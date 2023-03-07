ab8e3b7d55362d139fc7b300856b192aedf4a848
Heimsk <henriquemuller936@gmail.com>
2023-03-07 19:22:56 -0300

    feat(ui): insert all UI components in the interface

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,46 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { ChannelData } from "~/components/ChannelData";
+import { ChannelInfo } from "~/components/ChannelInfo";
+import { ChannelList } from "~/components/ChannelList";
+import { ServerList } from "~/components/ServerList";
+import { ServerName } from "~/components/ServerName";
+import { UserInfo } from "~/components/UserInfo";
+import { UserList } from "~/components/UserList";
 
 export default component$(() => {
   return (
     <>
-      <h1>Hello World!</h1>
+      <div style={{ display: "flex" }}>
+        <ServerList />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <ServerName />
+          <ChannelList />
+          <UserInfo />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            height: "100%",
+            flexDirection: "column",
+          }}
+        >
+          <ChannelInfo />
+          <ChannelData />
+          <div
+            style={{ position: "absolute", top: "46px", right: 0, bottom: 0 }}
+          >
+            <UserList />
+          </div>
+        </div>
+      </div>
     </>
   );
 });
@@ -14,8 +50,7 @@ export const head: DocumentHead = {
   meta: [
     {
       name: "description",
-      content:
-        "A lightweight, fast and customizable alternative to Discord official client.",
+      content: "Discord lightweight alternative.",
     },
   ],
 };
